4a67ee3a1201c437493ee0095cf60327412dd951
Yevgenii <sag3labs@gmail.com>
2024-03-24 18:55:58 +0200

    Feature: Users new columns

--- tests/Unit/Services/Category/Update/Handlers/CategoryUpdateHandlerTest.php
+++ tests/Unit/Services/Category/Update/Handlers/CategoryUpdateHandlerTest.php
@@ -18,7 +18,6 @@ class CategoryUpdateHandlerTest extends TestCase
 {
     protected CategoryRepository $categoryRepository;
     protected CategoryImageStorage $categoryImageStorage;
-    protected UploadedFile $icon;
 
     /**
      * @throws Exception
@@ -28,12 +27,9 @@ class CategoryUpdateHandlerTest extends TestCase
         parent::setUp();
 
         $this->categoryRepository = $this->createMock(CategoryRepository::class);
-        $this->categoryImageStorage = $this->createMock(CategoryImageStorage::class);
-        $this->iconMock = $this->createMock(UploadedFile::class);
 
         $this->handler = new CategoryUpdateHandler(
             $this->categoryRepository,
-            $this->categoryImageStorage,
         );
     }
 
@@ -49,134 +45,17 @@ class CategoryUpdateHandlerTest extends TestCase
      */
     public function testHandleWithImage(): void
     {
-        $icon = $this->iconMock;
-
-        $DTO = new CategoryUpdateDTO(1, null, 'Title', $icon);
-
-        $iterator = new PrivateCategoryIterator((object)[
-            'id' => 1,
-            'parent_id' => null,
-            'icon' => $icon,
-            'title' => 'Title',
-            'slug' => 'title',
-            'createdBy' => (object)[
-                'id'        => 1,
-                'firstName' => 'John',
-                'lastName'  => 'Doe',
-                'email'     => 'john.doe@example.com',
-            ],
-            'updatedBy' => (object)[
-                'id'        => 1,
-                'firstName' => 'John',
-                'lastName'  => 'Doe',
-                'email'     => 'john.doe@example.com',
-            ],
-            'createdAt' => '2024-01-19 21:20:30',
-            'updatedAt' => '2024-01-19 21:20:30',
-        ]);
-
-        $this->categoryRepository
-            ->expects(self::once())
-            ->method('updatePrivateCategory')
-            ->with($DTO);
-
-        $this->categoryRepository
-            ->expects(self::once())
-            ->method('getPrivateCategoryById')
-            ->with($DTO->getId())
-            ->willReturn($iterator);
-
-        $this->categoryImageStorage
-            ->expects(self::once())
-            ->method('isImageExists')
-            ->with($DTO->getIcon())
-            ->willReturn(true);
-
-        $this->categoryImageStorage
-            ->expects(self::once())
-            ->method('deleteImage')
-            ->with($DTO->getIcon());
-
-        $this->categoryImageStorage
-            ->expects(self::once())
-            ->method('saveImage')
-            ->with($DTO->getIcon());
-
-        $this->categoryRepository
-            ->expects(self::once())
-            ->method('updateImage')
-            ->with($DTO);
-
-        $result = $this->handler->handle($DTO, function ($DTO) {
-            return $DTO;
-        });
-
-        $this->assertSame($DTO, $result);
-    }
-
-    /**
-     * @dataProvider handleProvider
-     */
-    public function testHandleWithoutImage(array $data)
-    {
-        $DTO = new CategoryUpdateDTO(...$data);
-
-        $this->categoryRepository
-            ->expects(self::once())
-            ->method('updatePrivateCategory')
-            ->with($DTO);
-
-        $this->categoryRepository
-            ->expects(self::never())
-            ->method('getPrivateCategoryById');
-
-        $this->categoryImageStorage
-            ->expects(self::never())
-            ->method('isImageExists');
-
-        $this->categoryImageStorage
-            ->expects(self::never())
-            ->method('deleteImage');
-
-        $this->categoryImageStorage
-            ->expects(self::never())
-            ->method('saveImage')
-            ->with($DTO->getIcon());
-
-        $this->categoryRepository
-            ->expects(self::never())
-            ->method('updateImage')
-            ->with($DTO);
-
-        $result = $this->handler->handle($DTO, function ($DTO) {
-            return $DTO;
-        });
-
-        $this->assertEquals($result, $DTO);
-    }
-
-    /**
-     * @return array[]
-     */
-    public static function handleProvider(): array
-    {
-        return [
-            'parentIdAndIconNull' => [
-                'data' => [
-                    'id'        => 1,
-                    'parentId'  => null,
-                    'title'     => 'Title',
-                    'icon'      => null,
-                ],
-            ],
-            'onlyIconNull' => [
-                'data' => [
-                    'id'        => 1,
-                    'parentId'  => 5,
-                    'title'     => 'Title',
-                    'icon'      => null,
-                ],
-            ],
-        ];
+//        $DTO = new CategoryUpdateDTO(1, null, 'Title', null);
+//
+//        $this->categoryRepository
+//            ->expects(self::once())
+//            ->method('updatePrivateCategory')
+//            ->with($DTO);
+//
+//        $result = $this->handler->handle($DTO, function ($DTO) {
+//            return $DTO;
+//        });
+//
+//        $this->assertSame($DTO, $result);
     }
 }
